fcdeb1ffb3d2342cf2ac69c3cbf7ae1a496d59ee
Chareeef <youssef.charif.h@gmail.com>
2024-05-16 13:42:06 +0100

    Find and Memoize Primes <= N

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+"""Prime Game :
+Maria and Ben are playing a game. Given a set of consecutive integers starting
+from 1 up to and including n, they take turns choosing a prime number
+from the set and removing that number and its multiples from the set.
+The player that cannot make a move loses the game.
+
+They play x rounds of the game, where n may be different for each round.
+Assuming Maria always goes first and both players play optimally,
+our task is to determine who the winner of each game is.
+"""
+
+# Create an initial Sieve Of Eratosthenes from 0 to 2
+Sieve = [False, False, True]
+
+# Memoize initial primes less than or equal to the largest N so far
+N = 2
+Primes = {2: [2]}
+
+
+def isWinner(x, nums):
+    """Determine the winner of the Prime Game between Ben and Maria, where:
+        - x: the number of rounds
+        - nums: is an array of numbers, each `n` represent the maximum of the
+        `index + 1`-th round's set
+
+    Return: name of the player that won the most rounds,
+    and if the winner cannot be determined, return None
+    """
+    pass
+
+def findPrimes(n):
+    """From Sieve, Define, Memoize and Return the primes <= n
+    """
+
+    global Sieve
+    global Primes
+
+    print(f'For {n}\'s primes, define, Memoize and Return them')
+
+    # Define
+    n_primes = [i for i in range(n + 1) if Sieve[i] == True]
+
+    # Memoize
+    Primes[n] = n_primes
+
+    # Return
+    return n_primes
+
+
+def sieveOfEratosthenes(n):
+    """Return an array of all the prime numbers less than or equal to `n`
+    """
+
+    global Sieve
+    global N
+    global Primes
+
+    # If the current Sieve is sufficiently large
+    if n <= N:
+
+        print(f'Sieve large enough for {n}')
+        # If the prime <= n were already defined, return them
+        n_primes = Primes.get(n)
+        if n_primes:
+            print(f'{n}\'s primes already defined')
+            return n_primes
+
+        # If not: Define, Memoize and Return them
+        else:
+            return findPrimes(n)
+
+    # If n is beyond the current Sieve, expand it
+    else:
+        print(f'Sieve needs to be expanded for {n}')
+
+        # Append the needed amount of numbers and set them as True
+        for _ in range(N, n):
+            Sieve.append(True)
+
+        print('Updated length:', len(Sieve))
+
+        # Update N
+        N = n
+
+        # Mark the added composite numbers as False
+        i = 2
+        while i * i <= N:
+
+            # When a prime number is encountred, set its multiples to False
+            for m in range(i + i, N + 1, i):
+                Sieve[m] = False
+
+            # Increment `i`
+            i += 1
+
+        # Define, Memoize and Return the primes <= n
+        return findPrimes(n)
+        
+
+if __name__ == '__main__':
+    print('\tn = 2:')
+    print('2 =>', sieveOfEratosthenes(2))
+    print('\n\t===\n')
+
+    print('\tn = 10:')
+    print('10 =>', sieveOfEratosthenes(10))
+    print('\n\t===\n')
+
+    print('\tn = 5:')
+    print('5 =>', sieveOfEratosthenes(5))
+    print('\n\t===\n')
+
+    print('\tn = 50:')
+    print('50 =>', sieveOfEratosthenes(50))
+    print('\n\t===\n')
+
+    print('\tn = 10:')
+    print('10 =>', sieveOfEratosthenes(10))
+    print('\n\t===\n')
+
+    print('\tn = 50:')
+    print('50 =>', sieveOfEratosthenes(50))
+    print('\n\t===\n')
+
