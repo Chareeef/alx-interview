bb0243887a13cb1e215171c49b2739df0bc7514f
Chareeef <youssef.charif.h@gmail.com>
2024-05-16 17:53:35 +0100

    Refactor

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""Prime Game :
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number
-from the set and removing that number and its multiples from the set.
-The player that cannot make a move loses the game.
-
-They play x rounds of the game, where n may be different for each round.
-Assuming Maria always goes first and both players play optimally,
-our task is to determine who is the winner of each game.
+"""Prime Game
 """
 
 # Create an initial Sieve Of Eratosthenes from 0 (for better readability) to 2
@@ -31,6 +23,80 @@ def isWinner(x, nums):
     and if the winner cannot be determined, return None
     """
 
+    def getPrimes(n, useMax):
+        """Define, Memoize and Return the primes <= n
+        If `useMax` is True, retrieve primes from MaxPrimes,
+        and from Sieve otherwise
+        """
+
+        global Sieve
+        global Primes
+        global MaxPrimes
+
+        # Define
+        if useMax:
+
+            # Retrieve from already found primes stored in MaxPrimes
+            n_primes = [num for num in MaxPrimes if num <= n]
+
+        else:
+
+            # Retrieve from Sieve
+            n_primes = [i for i in range(n + 1) if Sieve[i] is True]
+
+            # Update MaxPrimes
+            MaxPrimes = n_primes
+
+        # Memoize
+        Primes[n] = n_primes
+
+        # Return
+        return n_primes
+
+    def findPrimes(n):
+        """Return an array of all the prime numbers less than or equal to `n`
+        """
+
+        global Sieve
+        global N
+        global Primes
+
+        # If the current Sieve is sufficiently large
+        if n <= N:
+
+            # If the prime <= n were already defined, return them
+            n_primes = Primes.get(n)
+            if n_primes:
+                return n_primes
+
+            # If not: Define, Memoize and Return them
+            else:
+                return getPrimes(n, True)
+
+        # If n is beyond the current Sieve, expand it
+        else:
+
+            # Append the needed amount of numbers and set them as True
+            for _ in range(N, n):
+                Sieve.append(True)
+
+            # Update N
+            N = n
+
+            # Mark the added composite numbers as False
+            i = 2
+            while i * i <= N:
+
+                # When a prime number is encountred, set its multiples to False
+                for m in range(i + i, N + 1, i):
+                    Sieve[m] = False
+
+                # Increment `i`
+                i += 1
+
+            # Define, Memoize and Return the primes <= n
+            return getPrimes(n, False)
+
     # Initialize scores
     Maria = 0
     Ben = 0
@@ -53,79 +119,3 @@ def isWinner(x, nums):
         return None
     else:
         return 'Maria' if Maria > Ben else 'Ben'
-
-
-def findPrimes(n):
-    """Return an array of all the prime numbers less than or equal to `n`
-    """
-
-    global Sieve
-    global N
-    global Primes
-
-    # If the current Sieve is sufficiently large
-    if n <= N:
-
-        # If the prime <= n were already defined, return them
-        n_primes = Primes.get(n)
-        if n_primes:
-            return n_primes
-
-        # If not: Define, Memoize and Return them
-    else:
-        return getPrimes(n, True)
-
-    # If n is beyond the current Sieve, expand it
-else:
-
-    # Append the needed amount of numbers and set them as True
-        for _ in range(N, n):
-            Sieve.append(True)
-
-        # Update N
-        N = n
-
-        # Mark the added composite numbers as False
-        i = 2
-        while i * i <= N:
-
-            # When a prime number is encountred, set its multiples to False
-            for m in range(i + i, N + 1, i):
-                Sieve[m] = False
-
-            # Increment `i`
-            i += 1
-
-        # Define, Memoize and Return the primes <= n
-        return getPrimes(n, False)
-
-
-def getPrimes(n, useMax):
-    """Define, Memoize and Return the primes <= n
-    If `useMax` is True, retrieve primes from MaxPrimes,
-    and from Sieve otherwise
-    """
-
-    global Sieve
-    global Primes
-    global MaxPrimes
-
-    # Define
-    if useMax:
-
-        # Retrieve from already found primes stored in MaxPrimes
-        n_primes = [num for num in MaxPrimes if num <= n]
-
-    else:
-
-        # Retrieve from Sieve
-        n_primes = [i for i in range(n + 1) if Sieve[i] is True]
-
-        # Update MaxPrimes
-        MaxPrimes = n_primes
-
-    # Memoize
-    Primes[n] = n_primes
-
-    # Return
-    return n_primes
